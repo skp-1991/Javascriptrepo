edd7a17b4101367ced0e4040a179c7da76afc9d4
skp-1991 <70481444+skp-1991@users.noreply.github.com>
2023-07-27 16:10:28 +0000

    conversaionoperation

---
+++ 01_basic_js/03_conversaionoperation.js
@@ -0,0 +1,29 @@
+console.log("==============")
+let score = 55
+console.log(typeof score)
+console.log(typeof(score))
+console.log("==============")
+let scoreTwo = "55"
+console.log(typeof scoreTwo)
+console.log(typeof(scoreTwo))
+console.log("==============")
+let valueScore = Number(scoreTwo)
+console.log(typeof valueScore)
+console.log("==============")
+let valueScoreOne = String(score)
+console.log(typeof valueScoreOne)
+console.log("==============")
+let scoreThree = "55abc"
+let valueScoreTwo = Number(scoreThree)
+console.log(typeof valueScoreTwo)
+console.log("If convert string to number and string have the alphabatical text the this will show in value NaN (Not a Number)")
+console.log(valueScoreTwo) 
+console.log("==============")
+let temprature = Number(null)
+console.log("If convert Null to number, then this will give the 0 value, this can be create the issue in if we are geting the temprature")
+console.log(temprature)
+
+console.log("==============")
+
+
+
