23dc199f63e1512c41c2360eb6aa97bc07640190
skp-1991 <70481444+skp-1991@users.noreply.github.com>
2023-07-27 15:45:32 +0000

    Datatype

--- 01_basic_js/02_datatype.js
+++ 01_basic_js/02_datatype.js
@@ -1,3 +1,24 @@
 "use strict"; // treat all JS code as newer version
 
-alert("hello")
+// Data type
+// number
+// string = "sushil"
+// boolen = true/false
+// null = standalone value null mean empty
+// undefine
+// symbol = use when work on react, unique
+// object
+
+
+let name = "sushil"
+let age = 31
+let ageOne = Number(31)
+let logIn = false
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof ageOne)
+console.log(typeof logIn)
+console.log(typeof null)
+console.log(typeof Object)
+
