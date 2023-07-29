963c7ade6005a713f72667dfc8ace758f272d33c
skp-1991 <70481444+skp-1991@users.noreply.github.com>
2023-07-29 07:44:41 +0000

    comparision

---
+++ 01_basic_js/04_comparision.js
@@ -0,0 +1,29 @@
+console.log(3 > 2)
+console.log(3 < 2)
+console.log(3 >= 2)
+console.log(3 != 2)
+console.log(3 == 2)
+
+
+console.log("=================")
+console.log("3" > 2)
+
+console.log(" This is not a good way=================")
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null != 0)
+
+console.log("This is not a good way =================")
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined != 0)
+
+
+console.log(" strictly check value =================")
+
+console.log(1 === 1)
+console.log("1" === 1)
