19bffc232fb679df63e3ef89c72e46617b50bda8
skp-1991 <70481444+skp-1991@users.noreply.github.com>
2023-07-31 15:43:31 +0000

    num

---
+++ 01_basic_js/08_num_maths.js
@@ -0,0 +1,13 @@
+let numValue = 87
+let numValueOne = new Number(87)
+console.log(numValue)
+console.log(typeof numValueOne)
+console.log(numValueOne)
+
+
+// max_safe_integer
+
+console.log(Number.MAX_SAFE_INTEGER + ' -  max_safe_integer')
+console.log(Number.MIN_SAFE_INTEGER + ' -  min_safe_integer')
+console.log(Number.MAX_VALUE + ' -  max_integer')
+console.log(Number.MIN_VALUE + ' -  min_integer')
