94d5f0a630e8fa0b5343208d10bd90986c5be418
skp-1991 <70481444+skp-1991@users.noreply.github.com>
2023-08-04 09:22:58 +0000

    array1

---
+++ 02_basic_js/11_array1.js
@@ -0,0 +1,9 @@
+const arrValue = [0,1,2,3,4,5]
+
+console.log(arrValue)
+console.log(arrValue[2])
+console.log(arrValue.length)
+
+
+// shallow property - if you will change the value then array will also change and Deep copy - this create a copy of the array this do not change in the orignal array.
+
