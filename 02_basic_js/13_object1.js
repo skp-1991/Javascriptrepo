16c81d84865fe6a877df83dc3f0f4a037d04f7bc
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-08-10 08:47:21 +0000

    object 2

--- 02_basic_js/13_object1.js
+++ 02_basic_js/13_object1.js
@@ -6,7 +6,11 @@
 const objValue = {
     name : "Sushil",
     Age : 31,
-    designation : "Website Designer"
+    designation : "Website Designer",
+    email : "sushil@sushil.com",
+    isLogined : false,
+    lastLogin : ["Monday","Tuesday","Wensday","Thusday"]
 }
 
-console.log(objValue);
+console.log(objValue.name)
+// console.log(objValue[name, age])
