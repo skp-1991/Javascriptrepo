f0a6d15936997a1b690263fd3e7defd195e24ec9
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-08-16 10:54:40 +0000

    object5

--- 02_basic_js/14_object2.js
+++ 02_basic_js/14_object2.js
@@ -31,6 +31,41 @@ const obj4 = {obj1,obj2}
 
 //console.log(obj4)
 
-const ob1 = Object.assign({}, obj1,obj2)
+// const ob1 = Object.assign({}, obj1,obj2)
+// const ob1 = {...obj1,...obj2} // spread operator
 
-console.log(ob1)
+// console.log(ob1)
+
+const iUser = [
+    {
+        email : "sushil@sushil.com",
+        phone : 9789845624
+    },
+    {
+        email : "sushil@sushil.com",
+        phone : 9789845624
+    },
+    {
+        email : "sushil@sushil.com",
+        phone : 9789845624
+    },
+    {
+        email : "sushil@sushil.com",
+        phone : 9789845624
+    },
+    {
+        email : "sushil@sushil.com",
+        phone : 9789845624
+    }
+]
+
+// we can use forecach 
+iUser.forEach(element => {
+    console.log(element.email)
+});
+
+
+console.log(Object.keys(obj1))  // this use for take keys from object
+console.log(Object.values(obj1))  // this use for take values from object
+console.log(Object.entries(obj1))  // this is use for covert the object to array.
+console.log(regularEmployee.hasOwnProperty('fullName'))  // this will provide the true or false if he find the key or value in object.
