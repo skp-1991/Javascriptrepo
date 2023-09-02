92e622ae63ce06b7ec5f60493f06060e8510b94d
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-09-02 10:15:19 +0000

    condition

---
+++ 04_control_flow/23_if_conditions.js
@@ -0,0 +1,55 @@
+// if
+
+// const isUserLoggedIn = true
+
+// if(isUserLoggedIn){
+//     console.log("this is true");
+// }else{
+//     console.log("This is false")
+// }
+
+
+// We use some earthmatic opeartor for condition
+// < , > , >= , <= , == , != , ===
+
+// if (2 < 3){
+//     console.log("Less Than")
+// }
+
+// if (3 > 2){
+//     console.log("Greater Than")
+// }
+
+// if (3 >= 3){
+//     console.log("Greater Than equal too")
+// }
+
+// if (3 <= 3){
+//     console.log("Less Than equal too")
+// }
+
+// if(3 != 2){
+//     console.log("Not Equal too")
+// }
+
+// this only check the value not data type
+// if (3 == "3"){
+//     console.log("double equal too")
+// }
+
+// this check value and data type both - this call strickt equal
+// if (3 === 3){
+// if (3 === "3"){
+//     console.log("tripal equal too")
+// }else{
+//     console.log("not")
+// }
+
+
+const temprature = 50
+
+if(temprature <= 50){
+    console.log("Temprature less then 50");
+}else{
+    console.log("Temprature greater then 50")
+}
