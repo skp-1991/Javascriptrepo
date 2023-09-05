19489cf17d005a8ecceca4423391b7c7524b7e16
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-09-05 07:56:38 +0000

    loop1

---
+++ 05_itrations/31_other_loop.js
@@ -0,0 +1,18 @@
+// If we will take the forEach in a variable the forEach do not return any thing and give undefine.
+
+// const coding = ['JS','HTML','CSS','Photoshop']
+
+// const value = coding.forEach((item)=>{
+//     // console.log(`${item}`);
+//     return item
+// })
+
+// console.log(value);
+
+
+// filter operation
+
+let myNumber = [1,2,3,4,5,6,7,8,9,10]
+
+const myNumb = myNumber.filter((nums)=> nums > 5 )
+console.log(myNumb);
