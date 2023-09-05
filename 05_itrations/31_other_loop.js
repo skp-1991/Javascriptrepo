0866fa874d19e242b5b6cb85c8f262244802ddcd
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-09-05 09:38:07 +0000

    loop 23

--- 05_itrations/31_other_loop.js
+++ 05_itrations/31_other_loop.js
@@ -14,5 +14,9 @@
 
 let myNumber = [1,2,3,4,5,6,7,8,9,10]
 
-const myNumb = myNumber.filter((nums)=> nums > 5 )
+// const myNumb = myNumber.filter((nums)=> nums > 5 )
+
+const myNumb = myNumber.filter((nums)=>{
+    return nums > 5
+})
 console.log(myNumb);
