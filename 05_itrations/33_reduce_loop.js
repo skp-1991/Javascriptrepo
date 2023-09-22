947ee34ae0302e8d5d9728f028c08fef146568bf
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-09-22 10:57:59 +0000

    reduce 2

--- 05_itrations/33_reduce_loop.js
+++ 05_itrations/33_reduce_loop.js
@@ -13,4 +13,33 @@ let inValue = 0
 let numbersAll = numbers.reduce((acc,curr)=> acc + curr , 0 )
 numbersAll = numbers.reduce((acc,curr)=> (acc + curr) , 0 )
 
-console.log(numbersAll);
+//console.log(numbersAll);
+
+
+
+let cource = [
+    {
+        courceName : "Mobile Repair",
+        price : 2500
+    },
+    {
+        courceName : "B.Tech",
+        price : 5000
+    },
+    {
+        courceName : "JS",
+        price : 1500
+    },
+    {
+        courceName : "React Js",
+        price : 4000
+    },
+    {
+        courceName : "PHP",
+        price : 6000
+    }
+]
+
+let Allcource = cource.reduce((accu,items) => (accu + items.price),0)
+
+console.log(Allcource);
