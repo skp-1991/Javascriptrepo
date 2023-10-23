1f9a893726e3dec4f5b13dd24a09a842224e3819
Sushil Kumar Pandey <70481444+skp-1991@users.noreply.github.com>
2023-10-23 08:42:53 +0000

    fatch

--- 09_advance/44_promises.js
+++ 09_advance/44_promises.js
@@ -42,42 +42,81 @@
 
 // fourth promise
 
-const fourthPromise = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = false
-        if (!error) {
-            resolve({username : 'Anil', email : 'anil@anil.com'})
-        }else{
-            reject('Error: something wrong')
-        }
-    }, 1000)
-})
+// const fourthPromise = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false
+//         if (!error) {
+//             resolve({username : 'Anil', email : 'anil@anil.com'})
+//         }else{
+//             reject('Error: something wrong')
+//         }
+//     }, 1000)
+// })
 
-fourthPromise.then(function(user){ // then use for get the data
-    console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch((err)=>{   // catch use for print the error 
-    console.log(err);
-}).finally(()=>{
-    console.log('promise resolved')
-})
+// fourthPromise.then(function(user){ // then use for get the data
+//     console.log(user);
+//     return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch((err)=>{   // catch use for print the error 
+//     console.log(err);
+// }).finally(()=>{
+//     console.log('promise resolved')
+// })
 
 
 // Promise five
 
-const promiseFive = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let err = true
-        if(!err){
-        resolve({username : 'Sunil', pass : '2323'})
-        }else{
-            reject('Error gone wrong')
-        }
-    },1000)
-})
+// const promiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let err = true
+//         if(!err){
+//         resolve({username : 'Sunil', pass : '2323'})
+//         }else{
+//             reject('Error gone wrong')
+//         }
+//     },1000)
+// })
 
-promiseFive.then(()=>{
+// This way we use only when we get the data without any error
+// async function consumePromiseFive(){
+//     const res = await promiseFive
+//     console.log(res)
+// }
+
+
+// If we have any error then we use try and catch in side this fucntion
+// async function consumePromiseFive(){
+//     try {
+//         const res = await promiseFive
+//     console.log(res)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// consumePromiseFive()
+
+
+
+// Promise Six
+
+// we will use await with the fatch becasue this is taking time to fatch the data
+async function allUserData(){
+    try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')  
+            // this data will come in string formate and we will convert this in JSON formate
+            const data = await response.json()
+            for (let index = 0; index < data.length; index++) {
+                console.log(`${data[index].address.geo.lat} - ${data[index].address.geo.lng} `)
+            }
+        } 
+    catch (error) {
+        console.log('E:', error)
+    }
     
-})
+}
+
+allUserData()
+
+
